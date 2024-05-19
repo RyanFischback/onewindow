1d7c151e7445728abbf527931f0b158709226545
rfischback <ryan-fischback@hotmail.com>
2024-05-19 20:08:00 -0300

    bleh

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>P1</li>
-          <li>P2</li>
-        </ul>
-      </nav>
       <div className="parent-one">
         <h1>Hello, World!</h1>
       </div>
---
+++ src/components/p1.tsx
@@ -0,0 +1,10 @@
+function P1() {
+  return (
+    <>
+      <div>
+        <img src="../assets/react.svg"></img>
+      </div>
+    </>
+  );
+}
+export default P1;
