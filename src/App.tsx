30f916a77e5f211078d35534f28008af161eeb63
rfischback <ryan-fischback@hotmail.com>
2024-05-19 18:44:57 -0300

    pushing project to see if i can use vscode in web for only one window while dev

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>P1</li>
+          <li>P2</li>
+        </ul>
+      </nav>
+      <div className="parent-one">
+        <h1>Hello, World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
